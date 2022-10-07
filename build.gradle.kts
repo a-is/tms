f0e59e742b0ffbe1abaad88dd1984b3c6a99e298
Alexey Ismagilov <al.a.ismagilov@gmail.com>
2022-10-08 00:32:46 +0300

    DEL: build: comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,6 @@ application {
     mainClass.set("tms.MainKt")
 }
 
-// See: https://stackoverflow.com/a/63767408/12479250
 tasks.jar {
     manifest.attributes["Main-Class"] = application.mainClass
 }
