e89fd16ee93b34bafeb6aca6f60c14d768bedb58
Alexey Ismagilov <al.a.ismagilov@gmail.com>
2022-10-19 11:28:37 +0300

    REF: help message

--- src/main/kotlin/tms/console/HelpCommand.kt
+++ src/main/kotlin/tms/console/HelpCommand.kt
@@ -1,50 +1,73 @@
 package tms.console
 
+import org.jline.reader.impl.completer.NullCompleter
+
 private val OFFSET: String = "    "
 
 class HelpCommand(
     commands: List<Command>
 ) : Command {
-    private var helpMessage: String
-
     override val name: String = "help"
 
-    override val description: String = "show this message"
+    override val description: String = "print help for commands"
 
+    // TODO: optional command name completer
     override val arguments: List<CommandArgument> = listOf()
+
+    private val helpMessage: String
+
+    private val commandsHelpMessage: Map<String, String>
 
     init {
         val commands = (commands + this).sortedBy { it.name }
 
         val helpBuilder = StringBuilder()
+        val commandsHelp = mutableMapOf<String, String>()
 
         helpBuilder.append("Commands:\n")
 
+        val maxCommandLength = commands.maxOfOrNull { it.name.length } ?: 0
+
         for (command in commands.sortedBy { it.name }) {
-            helpBuilder.append("$OFFSET${command.name}")
+            val commandFormattedName = String.format("%-${maxCommandLength}s", command.name)
 
-            for (argument in command.arguments) {
-                helpBuilder.append(" <${argument.name}>")
-            }
+            helpBuilder
+                .append(OFFSET)
+                .append(commandFormattedName)
+                .append(" - ")
+                .append(command.description)
+                .append('\n')
 
-            helpBuilder.append('\n')
+            val commandHelpBuilder = StringBuilder()
 
-            helpBuilder.append("$OFFSET$OFFSET${command.description}\n")
+            commandHelpBuilder.append(command.name).append('\n')
 
             val maxArgumentLength = command.arguments.maxOfOrNull { it.name.length } ?: 0
 
-            if (!command.arguments.isEmpty()) {
-                for (argument in command.arguments) {
-                    val formattedName = String.format("%-${maxArgumentLength}d", argument.name)
-                    helpBuilder.append("$OFFSET$OFFSET$formattedName - ${argument.description}")
-                }
+            for (argument in command.arguments) {
+                val argumentFormattedName = String.format("%-${maxArgumentLength}s", argument.name)
 
-                helpBuilder.append('\n')
+                commandHelpBuilder
+                    .append(OFFSET)
+                    .append(argumentFormattedName)
+                    .append(" - ")
+                    .append(argument.description)
+                    .append('\n')
             }
+
+            commandsHelp[command.name] = commandHelpBuilder.dropLast(1).toString()
         }
 
         helpMessage = helpBuilder.dropLast(1).toString()
+
+        commandsHelpMessage = commandsHelp
     }
 
-    override fun execute(args: List<String>): String = helpMessage
+    override fun execute(args: List<String>): String {
+        return if (args.size == 1) {
+            helpMessage
+        } else {
+            commandsHelpMessage[args[1]] ?: throw UnsupportedCommandException(args[1])
+        }
+    }
 }
